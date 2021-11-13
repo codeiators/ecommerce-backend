3a9cc70be445cae2e1e2f70a780d472663eab57b
Ashu jain <ashujain@Ashus-MacBook-Air.local>
2021-11-13 13:13:29 +0530

    added country and state repository

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -4,6 +4,7 @@ import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
